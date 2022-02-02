4c54ba8c4db3e165e80595f99416f0f7c82db08b
vMarhil <marxill@mail.ru>
2022-02-02 23:48:52 +0500

    №8

---
+++ №8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать четные числа от 1 до N
+Console.Clear();
+int[] Array  = {1,2,3,4,5,78,86,98,188};                             
+int n=Array.Length;
+int index=0;
+while(index<n)
+{
+    if(Array[index]%2==0)
+    {
+        Console.WriteLine(Array[index]);  
+    }
+    index++;
+}
