79adb21e9028fc813b94c53eb94495ecaa254313
vMarhil <marxill@mail.ru>
2022-02-02 23:37:01 +0500

    №4

---
+++ №4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти максимальное из трех чисел
+Console.Clear();
+int a = 10;
+int b = 8;
+int c = 33;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
