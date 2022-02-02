2375ab9ca05cb11867f20ec0026bff6f64061d03
vMarhil <marxill@mail.ru>
2022-02-02 23:36:03 +0500

    №2

---
+++ №2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Даны два числа. Показать большее и меньшее число
+Console.Clear();
+int a = 3;
+int b = 2;
+if (a>b)
+{
+  Console.WriteLine(" max a, min b");  
+}
+else
+{
+    Console.WriteLine(" max b, min a"); 
+}
