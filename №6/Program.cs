b4fb6a23b759c149c487078a59f9aaefdde361b3
vMarhil <marxill@mail.ru>
2022-02-02 23:48:11 +0500

    №6

---
+++ №6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным
+Console.Clear();
+int a;
+Console.Write("введите число: ");
+a= Console.Read();
+if (a % 2==1)
+{
+    Console.WriteLine("число нечетное");
+    }
+else
+{
+    Console.WriteLine("Число четное");
+}
+Console.ReadKey();
