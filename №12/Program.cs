2ec1f600c73d295548bd8ecca8e99d511c529445
vMarhil <marxill@mail.ru>
2022-02-06 14:39:57 +0500

    №12

---
+++ №12/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Clear(); 
+Console.WriteLine("введите трех значное чилсло: "); 
+int num = int.Parse(Console.ReadLine()); 
+int a = num / 100; 
+int b = num % 10;
+int c = a * 10 + b;
+Console.WriteLine (c);
