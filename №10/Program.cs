7260e2631c654ed0b3c941c01f71aca46deaa5e4
vMarhil <marxill@mail.ru>
2022-02-06 14:39:32 +0500

    №10

---
+++ №10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трёхзначного числа. 
+ 
+Console.Clear(); 
+Console.WriteLine("введите трех значное чилсло: "); 
+int num = int.Parse(Console.ReadLine()); 
+int n = num / 10 % 10; 
+Console.WriteLine("Второе число: " + n);
