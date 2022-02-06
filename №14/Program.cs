dbd97d3f1a2c925d243dae48f83c8e251f608e3f
vMarhil <marxill@mail.ru>
2022-02-06 14:40:11 +0500

    №14

---
+++ №14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Clear(); 
+Console.WriteLine("введите чилсло: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine("третьего числа нет");
+}
+while (num > 999 && num > 99)
+{
+    num = num / 10;  
+}
+if (num < 1000 && num > 99)
+{
+    int e = num % 10;
+    Console.WriteLine("третье число " + e);
+}
