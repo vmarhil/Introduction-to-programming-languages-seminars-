d0968134e9442f6e3a1a8d75690394b6c374862e
vMarhil <marxill@mail.ru>
2022-02-11 00:50:48 +0500

    №24

---
+++ №24/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти кубы чисел от 1 до N
+
+
+Console.Clear();
+Console.Write("Введите число:  ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Число:   ");
+for (int i = 1; i <= n; i++)
+{
+    Console.Write( i + "\t");
+}
+Console.WriteLine();
+Console.Write("Куб:     " );
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(i*i*i + "\t");
+}
