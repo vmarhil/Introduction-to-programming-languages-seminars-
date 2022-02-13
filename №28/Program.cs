10b7447648b9473dd2061e3bcdef66a64be0258e
vMarhil <marxill@mail.ru>
2022-02-13 19:28:08 +0500

    №28

---
+++ №28/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int s = 0;
+while (n > 0)
+{
+    s = s + n % 10;
+    n = n / 10;
+    
+}
+Console.WriteLine(s); 
