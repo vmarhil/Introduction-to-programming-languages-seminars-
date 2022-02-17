2f287f004f931c31b639a947153e4424cf3a8311
vMarhil <marxill@mail.ru>
2022-02-17 22:25:15 +0500

    №36

---
+++ №36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+Console.Clear();
+
+int[] mass = new int[7]; 
+int nechet = 0;
+int chet = 0;
+for (int i = 0; i < mass.Length ; i++) 
+{ 
+    mass[i] = new Random().Next(100, 999); 
+    Console.WriteLine();
+    Console.Write(mass[i]); 
+    Console.WriteLine();
+    if (mass[i] % 2==1)
+{
+    
+    nechet ++;
+    Console.WriteLine($"число нечетное {mass[i]}" );
+    
+}
+    else 
+{
+    chet ++;
+    Console.WriteLine($"число четное {mass[i]}");
+} 
+}
+Console.WriteLine();
+Console.WriteLine($" не четно число встретилась {nechet} раз" + "\t" );
+Console.WriteLine($" четно  число встретилась {chet} раз" + "\t" );
