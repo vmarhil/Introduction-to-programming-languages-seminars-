b94424eedd5847f85a5ba065f635ae6ec362bb59
vMarhil <marxill@mail.ru>
2022-02-17 22:24:53 +0500

    №38

---
+++ №38/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+Console.Clear();
+
+
+int[] mass = new int[5]; 
+int sumNeChet = 0;
+for (int i = 0; i < mass.Length ; i++) 
+{ 
+    mass[i] = new Random().Next(0, 10); 
+    Console.WriteLine(mass[i]); 
+    Console.WriteLine();
+    
+    if (i % 2 == 1)    //(mass[i] % 2 == 1)
+    {
+        sumNeChet = sumNeChet + mass[i];
+    }
+ 
+} 
+Console.Write("Сумма нечетных чисел " + sumNeChet);
