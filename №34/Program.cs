230d43a4dc619d6a0848f45b80989978eae3c43e
vMarhil <marxill@mail.ru>
2022-02-18 18:33:51 +0500

    №34

---
+++ №34/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу замену элементов массива на противоположные
+Console.Clear();
+int[] mass = new int[10]; 
+ 
+for (int i = 0; i < mass.Length ; i++) 
+{ 
+    mass[i] = new Random().Next(0,10); 
+    Console.Write(mass[i] + "\t");
+    Console.WriteLine(mass[i]*-1  ); 
+    
+} 
+Console.WriteLine(); 
