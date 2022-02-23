a9f9a68e894f575769b190ed4d6f9de4dd1eba53
vMarhil <marxill@mail.ru>
2022-02-23 18:56:26 +0500

    №44

---
+++ №44/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.Clear();
+Console.WriteLine("Введите точку b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите точку k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите точку b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите точку k2: ");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine();
+double x = (b2 - b1)/(k1 - k2);
+double y = k1*(b2-b1)/(k1-k2)+b1;
+Console.Write($"точка пересечения двух прямых: ({x};{y})");
