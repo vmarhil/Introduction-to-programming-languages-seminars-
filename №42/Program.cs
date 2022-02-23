99cd7e5c10e38942e566f5651a5c6e6a2f003f1c
vMarhil <marxill@mail.ru>
2022-02-23 18:56:47 +0500

    №42

---
+++ №42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Clear();
+int j = 0;
+Console.Write("Введите колличество элементов: ");
+int n = int.Parse(Console.ReadLine());
+int[] myArray = new int[n]; // 
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"Введите элемент по индексом {i}: ");
+    myArray[i] = int.Parse(Console.ReadLine()); ;
+}
+for (int i = 0; i < myArray.Length; i++)
+{
+    
+    if (myArray[i] > 0)
+    {
+        j++;
+    }
+
+
+}
+Console.WriteLine("Количество положительных чисел: ");
+Console.WriteLine(j);
