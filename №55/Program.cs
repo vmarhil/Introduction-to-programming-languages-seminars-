ffaf9274aefbdeedc188eb4d0dd527c43976bef9
vMarhil <marxill@mail.ru>
2022-02-27 20:51:04 +0500

    №55

---
+++ №55/Program.cs
@@ -0,0 +1,46 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int m = int.Parse(Console.ReadLine());
+int[,] array = new int[n, m];
+double[] summ = new double[m];
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        array[i, j] = new Random().Next(0, 10);  
+        Console.Write(array[i, j] + "\t ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < n; i++)                   
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(array[i, j] + "\t");
+        summ[j] += (array[i, j]);
+
+    }
+    Console.WriteLine();
+
+}
+Console.WriteLine("Сумма: ");
+for (int i = 0; i < array.GetLength(1); i++) 
+{
+    Console.Write(summ[i] + "\t");
+}
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое столбца: ");
+
+
+for (int i = 0; i < array.GetLength(1); i++)  
+{
+    Console.Write( summ[i] / m  + "\t");
+}
+
+
+
+
