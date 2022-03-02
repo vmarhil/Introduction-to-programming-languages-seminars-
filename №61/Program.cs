7f5bb9bcfcae93d2a531851789c116d178176fa7
vMarhil <marxill@mail.ru>
2022-03-02 23:17:24 +0500

    №61

--- №61/Program.cs
+++ №61/Program.cs
@@ -52,10 +52,16 @@ int a01 = array[0,1];
 int a10 = array[1,0];
 int a11 = array[1,1];
 
-int b00 = array[0,0];
-int b01 = array[0,1];
-int b10 = array[1,0];
-int b11 = array[1,1];
+int b00 = array2[0,0];
+int b01 = array2[0,1];
+int b10 = array2[1,0];
+int b11 = array2[1,1];
+int c1 = a00*b00;
+int c2 = a01*b10;
+int C = c1+c2;
+ Console.WriteLine(c1);
+ Console.WriteLine(c2);
+ Console.WriteLine(C);
 
 int[,] array3 = new int[2, 2];
 array3[0,0] = ((a00*b00) + (a01*b10));
@@ -65,7 +71,7 @@ array3[1,1] = ((a10*b01) + (a11*b11));
 
 for (int i = 0; i < array3.GetLength(0); i++)
 {
-    for (int j = 0; j < array3.GetLength(1); j++)
+    for (int j = 0; j < array3.GetLength(1) ; j++)
     {
         Console.Write(array3[i,j] + "\t");
     }
