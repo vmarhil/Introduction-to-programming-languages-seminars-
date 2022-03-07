83f38635f3234eee6194a12b1a781277d229d456
vMarhil <marxill@mail.ru>
2022-03-07 18:30:32 +0500

    №69

---
+++ №69/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+Console.Clear();
+Console.WriteLine();
+
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+int SUMM(int M, int N)
+{
+    int summ = M;
+    if (M < N)
+    {
+        summ += SUMM(M + 1, N);
+    }
+    return summ;
+}
+
+int result = SUMM(M, N);
+Console.WriteLine($"Сумма элементов от {M} до {N} = {result}");
