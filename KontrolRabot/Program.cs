1fce259da89ddd25822b15888b90491666466623
vMarhil <marxill@mail.ru>
2022-04-01 23:42:43 +0500

    Kod2

--- KontrolRabot/Program.cs
+++ KontrolRabot/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] myArray = new string[5] {"432", "43", "Hoho", "war", ":=O"};
+string[] Array2 = new string[myArray.Length];
+void M1(string[] myArray, string[] Array2)
+{
+    int count = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+    if(myArray[i].Length <= 3)
+        {
+        Array2[count] = myArray[i];
+        count++;
+        }
+    }
+}
+void M2(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+M1(myArray, Array2);
+M2(Array2);
+
